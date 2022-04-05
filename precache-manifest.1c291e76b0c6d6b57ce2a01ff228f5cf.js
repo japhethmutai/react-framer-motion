e8b710a9839a1f68196b9e17366fbff19ab796c2
Japheth <japhemchirie@gmail.com>
2022-04-05 20:42:04 +0300

    Updates

---
+++ precache-manifest.1c291e76b0c6d6b57ce2a01ff228f5cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdcc20c551faf844926686800623e77",
+    "url": "/react-framer-motion/index.html"
+  },
+  {
+    "revision": "383feb7506ac728de27d",
+    "url": "/react-framer-motion/static/css/main.b7c9dd41.chunk.css"
+  },
+  {
+    "revision": "0cedb0ce819cd5c6acdc",
+    "url": "/react-framer-motion/static/js/2.48bb8971.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-framer-motion/static/js/2.48bb8971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383feb7506ac728de27d",
+    "url": "/react-framer-motion/static/js/main.66df926c.chunk.js"
+  },
+  {
+    "revision": "ab4ec3d7e7883177fa53",
+    "url": "/react-framer-motion/static/js/runtime-main.898d55db.js"
+  }
+]);
